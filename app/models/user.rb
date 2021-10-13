cf5a3b13c61dd496249b9bb6413a0344aca408e8
nori35 <spzx34e@yahoo.co.jp>
2021-10-13 21:32:49 +0900

    新規登録修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :email, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
-  validates :profile, presence: true
-  validates :user_id, presence: true
-  validates :user, presence: true
+  validates :position, presence: true
 
   has_many :prototypes
   has_many :comments
